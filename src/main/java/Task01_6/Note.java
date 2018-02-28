cc82e8682aeeccac304af4441770254990f9bde0
Vsevolod Beshanov <31348620+serevarno@users.noreply.github.com>
2018-03-01 01:50:36 +0300

    Implementation of deleteNote(int index) method Constructor of Note

--- src/main/java/Task01_6/Note.java
+++ src/main/java/Task01_6/Note.java
@@ -1,5 +1,17 @@
 package Task01_6;
 
 public class Note {
-    public String text;
+    private String text;
+
+    public Note(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
--- src/main/java/Task01_6/Notebook.java
+++ src/main/java/Task01_6/Notebook.java
@@ -1,19 +1,49 @@
 package Task01_6;
 
 public class Notebook {
-    Note[] notes = new Note[0];
+    Note[] noteArray;
+    Notebook(){
+        this.noteArray = new Note[0];
+    }
 
-    void addNote(Note newNote){
-        Note[] newNoteArray = new Note[notes.length+1];
-        for(int i = 0; i < notes.length; i++){
-            newNoteArray[i] = notes[i];
+    void addNote(Note newNote) {
+        Note[] newArray = new Note[noteArray.length + 1];
+        for (int i = 0; i < noteArray.length; i++) {
+            newArray[i] = noteArray[i];
         }
-        newNoteArray[newNoteArray.length-1] = newNote;
-        this.notes = newNoteArray;
+        newArray[newArray.length - 1] = newNote;
+        this.noteArray = newArray;
+    }
 
-        for(Note note : notes){
-            System.out.println(note.text);
+    void deleteNote(int index){
+        Note[] newArray = new Note[noteArray.length];
+        for(int i = 0; i < noteArray.length; i++){
+            if(i != index)
+                newArray[i] = noteArray[i];
+        }
+        this.noteArray = new Note[0];
+        for(Note note: newArray){
+            if(note != null){
+                addNote(note);
+            }
         }
     }
 
+    public static void main(String[] args) {
+        Notebook notebook = new Notebook();
+
+        Note note1 = new Note("Some text");
+        Note note2 = new Note("New text");
+
+        notebook.addNote(note1);
+        notebook.addNote(note1);
+        notebook.addNote(note2);
+
+        notebook.deleteNote(1);
+        System.out.println(notebook.noteArray.length);
+
+        for(Note note: notebook.noteArray){
+            System.out.println(note.getText());
+        }
+    }
 }
