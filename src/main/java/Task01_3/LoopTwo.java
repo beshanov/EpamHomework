96d437a5801dce206c5c84d96b324ecd90553f46
Vsevolod Beshanov <31348620+serevarno@users.noreply.github.com>
2018-02-28 15:19:28 +0300

    Initial commit

---
+++ src/main/java/Task01_3/LoopTwo.java
@@ -0,0 +1,24 @@
+package Task01_3;
+
+import static java.lang.Math.tan;
+
+public class LoopTwo {
+
+    public static double calculateFunc(double x) {
+        return tan(2 * x) - 3;
+    }
+
+    public void getTable(double a, double b, double step) {
+        double x = a;
+        while (x < b) {
+            System.out.println(x + "    " + calculateFunc(x));
+            x += step;
+        }
+    }
+
+    public static void main(String[] args) {
+        LoopTwo loop = new LoopTwo();
+        loop.getTable(2.0, 3.0, 0.05);
+    }
+
+}
---
+++ src/main/java/Task01_4/OneDimArray.java
@@ -0,0 +1,33 @@
+package Task01_4;
+
+public class OneDimArray {
+    private double[] array;
+
+    public OneDimArray(int size, double borderValue) {
+        array = new double[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random() * borderValue;
+        }
+    }
+
+    public double findMax() {
+        double max = 0;
+        double sum;
+        for (int i = 0; i < array.length / 2 + 1; i++) {
+            sum = array[i] + array[array.length - 1 - i];
+            if (sum > max)
+                max = sum;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        OneDimArray newArray = new OneDimArray(5, 6.0);
+        for (double i : newArray.array) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n" + newArray.findMax());
+    }
+
+
+}
